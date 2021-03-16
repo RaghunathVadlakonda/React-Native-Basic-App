5f1b91fc8e22ca16278b80fbbb2e3396a94cb456
Raghunath <Vadlakonda.raghu5@gmail.com>
2021-03-16 21:04:05 +0530

    Add input text field and button

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Welcome React-Native App')
+  const [inputText, setInputText] = useState('');
+
+  const updateInputText = (enterText) => {
+    setInputText(enterText);
+  }
+
+  const addUpdatedText = () => {
+    console.log("text updated!");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Click Me" onPress={() => setOutputText('After rendering this msg will show!')}/>
-      {/* <StatusBar style="auto" /> */}
+    <View>
+      <View style={styles.container}>
+        <TextInput 
+         placeholder="Please Enter Your Text" 
+         style={styles.inputField}
+         onChangeText={updateInputText}
+         value={inputText}
+          />
+
+        <Button title="Add" onPress={addUpdatedText} color="dodgerblue"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 50,
+    paddingHorizontal: 40,
+    paddingLeft: 20,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
+  inputField: {
+    borderColor: "black",
+    borderRadius: 5,
+    borderWidth: 1.5,
+    width: '80%',
+    padding: 1
+  }
 });
