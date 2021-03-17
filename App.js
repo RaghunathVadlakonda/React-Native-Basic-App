70edab4f8f89b60a88e489f4250a5d67e72d62ee
Raghunath <Vadlakonda.raghu5@gmail.com>
2021-03-17 19:23:04 +0530

    Add displayed the list of text fields and added scroll view

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [inputText, setInputText] = useState('');
+  const [textList, setTextList] = useState([]);
 
   const updateInputText = (enterText) => {
     setInputText(enterText);
   }
 
   const addUpdatedText = () => {
-    console.log("text updated!");
+    // console.log("text updated!");
+    setTextList((enteredText) => [...enteredText, inputText])
   }
 
   return (
@@ -25,23 +27,33 @@ export default function App() {
 
         <Button title="Add" onPress={addUpdatedText} color="dodgerblue"/>
       </View>
+      <ScrollView>
+        {textList.map(text => <View key={text} style={styles.texts}><Text>{text}</Text></View>)}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 50,
+    paddingTop: 50,
+    paddingBottom: 10,
     paddingHorizontal: 40,
     paddingLeft: 20,
     flexDirection: "row",
     justifyContent: "space-between"
   },
   inputField: {
+    padding: 1,
     borderColor: "black",
     borderRadius: 5,
     borderWidth: 1.5,
     width: '80%',
-    padding: 1
+  },
+  texts: {
+    padding: 10,
+    backgroundColor: "blue",
+    borderColor: "black",
+    margin: 10
   }
 });
